4ea1c2e1cba347e61d2c12c0365d989b4638c976
jamie pryce <djpusha876@gmail.com>
2023-09-25 16:56:39 -0400

    Made Changes to tests

--- backend/tests/TestCounter.cs
+++ backend/tests/TestCounter.cs
@@ -16,14 +16,14 @@ namespace tests
         private readonly ILogger logger = TestFactory.CreateLogger();
 
         [Fact]
-        public async void Http_trigger_should_return_known_string()
+        public void Http_trigger_should_return_known_string()
         {
             var counter = new Company.Function.Counter();
             counter.Id = "1";
             counter.Count = 2;
             var request = TestFactory.CreateHttpRequest();
-            var response = (HttpResponseMessage) Company.Function.GetResumeCounter.Run(request, counter, out counter, logger);
-            Assert.Equal(4, counter.Count);
+            var response = (HttpResponseMessage)Company.Function.GetResumeCounter.Run(request, counter, out counter, logger);
+            Assert.Equal(3, counter.Count);
         }
 
     }
